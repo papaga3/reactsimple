684b0c0237dfe1bfa0307072c52c6ce7e1b0eaeb
Nguyen <e1601140@edu.vamk.fi>
2019-03-21 09:26:52 +0200

    person.js edited

--- src/Person.js
+++ src/Person.js
@@ -1,8 +1,24 @@
 import React, {Component} from 'react';
 
-var myPerson={"id":"e1601140", "name":"Nguyen Quoc Huy"}
+//var myPerson={"id":"e1601140", "name":"Nguyen Quoc Huy"}
 //var myPerson
 class Person extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            person:{}
+        }
+    }
+
+    async componentDidMount(){
+        //read the file person.js from folder public
+        const personRemote = await( 
+            await fetch('person.json')).json();
+        //Overwrite the empty person in constructor with the
+        //data from the file
+        this.setState({person : personRemote});
+    }    
+
     render(){
         return(
             <div>
